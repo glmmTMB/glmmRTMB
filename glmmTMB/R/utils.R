d76c20efedfd0021ee7c10d0ceb6cc72f7397de8
DivitP <divitpurwar@gmail.com>
2026-07-30 18:35:10 -0700

    set useRTMB flag as true

--- glmmTMB/R/utils.R
+++ glmmTMB/R/utils.R
@@ -4,7 +4,7 @@
 #' @param flag FALSE (default) to use TMB backend; TRUE to use RTMB backend
 #' @export
 useRTMB <- local({
-  useRTMB <- FALSE
+  useRTMB <- TRUE
   function(flag = NULL) { useRTMB <<- flag %||% useRTMB ; useRTMB }
 })
 MakeADFun <- function(data, ..., DLL) {
